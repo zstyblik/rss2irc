zstyblik/rss2irc

commit 1d52e66b629e77ddfc8eb07e7601e9b346182540

Use feedparser in order to parse Atom feeds
Commit drops xml.etree in favour of feedparser in order to parse Atom feeds and
have everything unified. All hopes in reliability of feedparser.

rss2irc.py
@@ -3,6 +3,7 @@
 Desc: Fetch RSS and pipe it into IRC bot.
 """
 import argparse
+import feedparser
 import logging
 import logging.handlers
 import os
@@ -12,7 +13,6 @@ import signal
 import sys
 import time
 import traceback
-import xml.etree.ElementTree as ET
 
 EXPIRATION = 86400
 
@@ -22,6 +22,7 @@ def get_rss(url):
         rsp = requests.get(url, timeout=30)
         rsp.raise_for_status()
         data = rsp.text
+        rsp.close()
         logging.debug('Got RSS data.')
     except Exception:
         logging.debug('Failed to get RSS data.')
@@ -94,18 +95,15 @@ def parse_args():
 def parse_news(data):
     """Parse-out link and title out of XML."""
     news = {}
-    tree = ET.fromstring(data.encode('utf-8'))
-    for item in tree.iter('item'):
-        news_item = {}
-        for child in item.getchildren():
-            if child.tag in ['category', 'link', 'title']:
-                news_item[child.tag] = child.text
+    feed = feedparser.parse(data)
+    for entry in feed['entries']:
+        link = entry.pop('link', None)
+        title = entry.pop('title', None)
+        if not 'link' and not 'title':
+            continue
 
-        if 'link' in news_item and 'title' in news_item:
-            link = news_item.pop('link')
-            title = news_item.pop('title')
-            category = news_item.pop('category', None)
-            news[link] = (title, category)
+        category = entry.pop('category', None)
+        news[link] = (title, category)
 
     return news
 

commit 2af4df9a7da098412b78b7753df08e929a386e72

Fix bug in parse_news() - entries without link not being filtered out
I don't know what I was doing, but code in question doesn't make sense.
Broken by commit 1d52e66.

phpbb2slack.py
@@ -247,14 +247,15 @@ def parse_news(data: str, authors: List[str]) -> Dict:
     feed = feedparser.parse(data)
     for entry in feed["entries"]:
         link = entry.pop("link", None)
-        title = entry.pop("title", None)
-        author_detail = entry.pop("author_detail", {"name": None})
-        if not "link" and not "title":
+        if not link:
+            # If we don't have a link, there is nothing we can do.
             continue
 
+        author_detail = entry.pop("author_detail", {"name": None})
         if authors and author_detail["name"] not in authors:
             continue
 
+        title = entry.pop("title", "No title")
         category = entry.pop("category", None)
         comments_cnt = entry.pop("slash_comments", 0)
         try:

rss2irc.py
@@ -272,10 +272,11 @@ def parse_news(data: str) -> Dict[str, Tuple[str, str]]:
     feed = feedparser.parse(data)
     for entry in feed["entries"]:
         link = entry.pop("link", "")
-        title = entry.pop("title", "")
-        if not "link" and not "title":
+        if not link:
+            # If we don't have a link, there is nothing we can do.
             continue
 
+        title = entry.pop("title", "No title")
         category = entry.pop("category", "")
         news[link] = (title, category)
 

tests/files/phpbb-rss.xml
@@ -29,5 +29,13 @@ Does anyone else have the same situation?]]></content:encoded>
       <content:encoded><![CDATA[I ordered a new something yesterday and still have not received an email. i have emailed twice somewhere and still nothing....so now trying something.]]></content:encoded>
       <slash:comments>1</slash:comments>
     </item>
+    <item>
+      <title>Broken entry</title>
+      <pubDate>Fri, 14 Feb 2020 20:50:36 +0000</pubDate>
+      <author>invalid@example.com (otherUser)</author>
+      <dc:creator>otherUser</dc:creator>
+      <content:encoded><![CDATA[I ordered a new something yesterday and still have not received an email. i have emailed twice somewhere and still nothing....so now trying something.]]></content:encoded>
+      <slash:comments>1</slash:comments>
+    </item>
   </channel>
 </rss>

tests/files/rss.xml
@@ -19,5 +19,9 @@
             <description>Item2 description</description>
             <pubDate>Sun, 02 Feb 2020 11:26:26 -0500</pubDate>
         </item>
+        <item>
+            <description>Item3 description - purposefully broken</description>
+            <pubDate>Sun, 02 Feb 2020 11:25:26 -0500</pubDate>
+        </item>
     </channel>
 </rss>

tests/test_phpbb2slack.py
@@ -331,6 +331,29 @@ def test_main_cache_hit(
     assert len(fixture_http_server.requests) == 0
 
 
+def test_parse_news():
+    """Test parse_news()."""
+    expected_news = {
+        "https://phpbb.example.com/threads/something-of-something.424837/": {
+            "title": "Some other problem",
+            "category": None,
+            "comments_cnt": 0,
+        },
+        "https://phpbb.example.com/threads/something-not-received.424836/": {
+            "title": "Something not received",
+            "category": None,
+            "comments_cnt": 1,
+        },
+    }
+
+    rss_fname = os.path.join(SCRIPT_PATH, "files", "phpbb-rss.xml")
+    with open(rss_fname, "rb") as fhandle:
+        rss_data = fhandle.read().decode("utf-8")
+
+    result = phpbb2slack.parse_news(rss_data, [])
+    assert result == expected_news
+
+
 @pytest.mark.parametrize(
     "cache,expected_cache",
     [

tests/test_rss2irc.py
@@ -337,6 +337,27 @@ def test_main_cache_hit(
     assert sorted(output) == sorted(expected_output)
 
 
+def test_parse_news():
+    """Test parse_news()."""
+    expected_news = {
+        "http://www.example.com/scan.php?page=news_item&px=item1": (
+            "Item1",
+            "",
+        ),
+        "http://www.example.com/scan.php?page=news_item&px=item2": (
+            "Item2",
+            "",
+        ),
+    }
+
+    rss_fname = os.path.join(SCRIPT_PATH, "files", "rss.xml")
+    with open(rss_fname, "rb") as fhandle:
+        rss_data = fhandle.read().decode("utf-8")
+
+    result = rss2irc.parse_news(rss_data)
+    assert result == expected_news
+
+
 def test_scrub_items():
     """Test scrub_items()."""
     logging.basicConfig(level=logging.CRITICAL)