559e0193b5a658e7b7939b9bbbc76a7f8d7c5ec8
Zdenek Styblik <stybla@turnovfree.net>
2025-12-16 21:50:38 +0100

    List all imports in ci/run-flake8.sh
    One day I'm going to switch this project to src/ layout. However, since
    nobody is using it, since there is no need to publish to pypi, incentive
    to doing so is somewhat low.

--- ci/run-flake8.sh
+++ ci/run-flake8.sh
@@ -7,7 +7,7 @@ cd "$(dirname "${0}")/.."
 python3 -m flake8 \
     . \
     --ignore=W503 \
-    --application-import-names="app,settings" \
+    --application-import-names="cache_stats,gh2slack,git_commits2slack,phpbb2slack,rss2irc,rss2slack,lib" \
     --import-order-style=pycharm \
     --max-line-length=80 \
     --show-source \
